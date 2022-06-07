d507191672ff926b0cae05def7ad2ce0ad8a0294
Anton Saveliev <asaveliev@sberhealth.ru>
2022-06-07 15:17:46 +0300

    edit rebase.sh

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -1,7 +1,9 @@
 fy command line options
 
 count=1
-for param in "$*"; do
-    echo "\$* Parameter #$count = $param"
+for param in "$@"; do
+    echo "\$@ Parameter #$count = $param"
     count=$(( $count + 1 ))
 done
+
+echo "====="
